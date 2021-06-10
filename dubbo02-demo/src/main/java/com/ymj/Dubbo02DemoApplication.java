2222691bf69401bc2ae6912570fd703ef8231953
develop3 <2513667783@qq.com>
2021-06-10 23:26:47 +0800

    添加dubbo配置

---
+++ dubbo02-demo/src/main/java/com/ymj/Dubbo02DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ymj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dubbo02DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dubbo02DemoApplication.class, args);
+    }
+
+}
---
+++ dubbo02-demo/src/test/java/com/ymj/dubbo02demo/Dubbo02DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymj.dubbo02demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dubbo02DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
