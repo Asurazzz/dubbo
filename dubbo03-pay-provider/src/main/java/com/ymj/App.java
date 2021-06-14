636a883981ba2e999536f5c8ebdbe02fd6ef96ab
develop3 <2513667783@qq.com>
2021-06-14 11:55:34 +0800

    添加dubbo使用Zookeeper注册中心

--- dubbo03-pay-provider/src/main/java/com/ymj/App.java
+++ dubbo03-pay-provider/src/main/java/com/ymj/App.java
@@ -1,6 +1,6 @@
 package com.ymj;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.dubbo.container.Main;
 
 import java.io.IOException;
 
@@ -11,9 +11,11 @@ import java.io.IOException;
 public class App 
 {
     public static void main( String[] args ) throws IOException {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext =
-                new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/application.xml"});
-        classPathXmlApplicationContext.start();
-        System.in.read();
+//        ClassPathXmlApplicationContext classPathXmlApplicationContext =
+//                new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/application.xml"});
+//        classPathXmlApplicationContext.start();
+//        System.in.read();
+        // dubbo提供的启动类方法，会启动dubbo配置的多个container
+        Main.main(args);
     }
 }
